ed7e8b837eb9677ad5e739c56f51f58a79cb20eb
Yonnatan Bustos <ybustos@heinsohn.com.co>
2022-09-19 00:18:06 -0500

    Ajuste con el nombre del navbar

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import { Footer } from "../components/Footer"
-import { NavBar } from "../components/Navbar"
+import { NavBar } from "../components/NavBar"
 import { BriefcasePage } from "./BriefcasePage"
 import { ContactPage } from "./Contactpage"
 import { ExperiencePage } from "./ExperiencePage"
